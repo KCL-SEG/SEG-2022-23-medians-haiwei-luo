0e0a072bcbdf0fb9e183632366537eec4860e6a1
haiwei-luo <haiwei8809@gmail.com>
2023-10-05 18:12:30 +0100

    Complete the median exercise for an unsorted list

--- median.py
+++ median.py
@@ -1,12 +1,25 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import statistics
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        sortedNum = sorted(numbers)
+        lengthList = int(len(sortedNum))
+        if (lengthList % 2) != 0:
+            indexMedian = int((lengthList - 1) / 2)
+            median = sortedNum[indexMedian]
+        else:
+            indexMedianUpper = int(lengthList / 2)
+            indexMedianLower = indexMedianUpper - 1
+            median = statistics.mean([sortedNum[indexMedianUpper],sortedNum[indexMedianLower]])
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
 print(numbers)
+#print(sortedNum)
+print(f"This is the median: {median}")
